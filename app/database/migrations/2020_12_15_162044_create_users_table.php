6669a71cb34a4ac6059c19937987eb0d98154d3d
Trey <phlasc@gmail.com>
2020-12-15 11:28:01 -0500

    Create user migration

---
+++ app/database/migrations/2020_12_15_162044_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_repo_id');
+            $table->string('username');
+            $table->boolean('darkmode');
+            $table->integer('role')->references('id')->on('users_access');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
